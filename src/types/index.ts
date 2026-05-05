9a3888ca539f175b771ebbacd7b9267dd0e4d57f
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-05 10:13:16 +0200

    feat(types): add Country interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,27 @@
-// Types à définir au kickoff
+type Currency = {
+  code: string;
+  name: string;
+  symbol: string;
+};
+
+type Language = {
+  iso639_1: string;
+  iso639_2: string;
+  name: string;
+  nativeName: string;
+};
+
+export type Country = {
+  name: string;
+  topLevelDomain: string[];
+  alpha3Code: string;
+  capital: string;
+  subregion: string;
+  region: string;
+  population: number;
+  borders?: string[];
+  nativeName: string;
+  currencies: Currency[];
+  languages: Language[];
+  flag: string;
+};
