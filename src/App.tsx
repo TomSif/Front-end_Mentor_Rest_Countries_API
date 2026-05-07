c4d86ac2183bccab306f51e1de3d89de6245457d
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-07 11:52:23 +0200

    refactor(app): move Header from Home to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router";
+import Header from "./components/Header";
 import Home from "./components/Home";
 import CountryDetail from "./components/CountryDetail";
 import Fallback from "./components/Fallback";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/country/:alpha3Code" element={<CountryDetail />} />
-      <Route path="*" element={<Fallback />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/country/:alpha3Code" element={<CountryDetail />} />
+        <Route path="*" element={<Fallback />} />
+      </Routes>
+    </>
   );
 }
 
