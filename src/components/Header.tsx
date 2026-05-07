5cc69127e4c04562b283847aecba91ff405dfca7
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-07 11:15:29 +0200

    feat(header): add component with dark mode toggle

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import useTheme from "../hook/useTheme";
+
+function Header() {
+  const { isDark, toggleTheme } = useTheme();
+  return (
+    <header className="text-grey-950 border-b-grey-350 flex w-full flex-row justify-between border-2 border-transparent bg-white px-4 py-7.5 dark:bg-blue-900 dark:text-white">
+      <h1 className="text-preset-7">Where in the world?</h1>
+      <button
+        onClick={toggleTheme}
+        type="button"
+        className="text-preset-6-semibold cursor-pointer"
+      >
+        {isDark ? (
+          <span className="flex flex-row items-center gap-2">
+            <img
+              className="h-3 w-3"
+              src="/images/LightModeSun.png"
+              alt="sun symbol "
+            />
+            <span>Light Mode</span>
+          </span>
+        ) : (
+          <span className="flex flex-row items-center gap-2">
+            <img
+              className="h-3 w-3"
+              src="/images/DarkModeIcon.png"
+              alt="moon symbol "
+            />
+            <span>Dark Mode</span>
+          </span>
+        )}
+      </button>
+    </header>
+  );
+}
+
+export default Header;
