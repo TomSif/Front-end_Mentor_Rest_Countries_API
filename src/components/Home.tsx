81adbd57b04486cb2ef56ac1403ae17c9cb9699a
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-08 12:52:51 +0200

    feat(home): render filtered list of country cards

---
+++ src/components/Home.tsx
@@ -0,0 +1,76 @@
+import { useState, useMemo } from "react";
+import { Region } from "../types";
+import useCountries from "../hook/useCountries";
+import CountryCard from "./CountryCard";
+
+const REGIONS: Region[] = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
+
+function Home() {
+  const [searchInput, setSearchInput] = useState<string>("");
+  const [filter, setFilter] = useState<Region>("");
+  const { countries } = useCountries();
+
+  const filteredCountries = useMemo(() => {
+    return countries.filter((country) => {
+      return (
+        (filter === "" || country.region === filter) &&
+        (searchInput === "" ||
+          country.name.toLowerCase().includes(searchInput.toLowerCase()))
+      );
+    });
+  }, [countries, filter, searchInput]);
+
+  return (
+    <main className="bg-grey-50 px-4 py-6 xl:px-21 dark:bg-blue-950">
+      <fieldset className="text-grey-950 mt-5 flex w-full flex-col gap-2 md:flex-row md:justify-between dark:text-white">
+        <legend className="sr-only">Filtres</legend>
+        <div className="relative w-full">
+          <span className="absolute top-1/2 left-8 -translate-y-1/2">
+            <img src="/images/Shape.png" alt="" />
+          </span>
+          <input
+            id="search"
+            type="text"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            placeholder="Search for a country"
+            className="w-full bg-white py-4 pr-4 pl-18 shadow-md dark:bg-blue-900"
+          />
+        </div>{" "}
+        <label className="sr-only" htmlFor="regions">
+          Filter by Region
+        </label>
+        <select
+          className="bg-white px-8 py-4 pr-11 shadow-md dark:bg-blue-900"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value as Region)}
+          id="regions"
+        >
+          <option value="">Filter by Region</option>
+          {REGIONS.map((region) => (
+            <option className="pt-4" key={region} value={region}>
+              {region}
+            </option>
+          ))}
+        </select>
+      </fieldset>
+      <section className="mt-11">
+        <ul className="grid w-full grid-cols-1 justify-items-center gap-10 gap-x-18 gap-y-18 px-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:px-0">
+          {filteredCountries.map((country) => (
+            <li key={country.alpha3Code}>
+              <CountryCard
+                name={country.name}
+                capital={country.capital}
+                region={country.region}
+                population={country.population}
+                flag={country.flag}
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default Home;
