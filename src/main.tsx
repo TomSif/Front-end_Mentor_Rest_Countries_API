34052bbea4bbf541bc799503333c4d96e6133be5
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-08 12:53:45 +0200

    feat(app): add countries and theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { CountriesProvider } from "./context/CountriesProvider.tsx";
+import { ThemeProvider } from "./context/ThemeProvider.tsx";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider>
+        <CountriesProvider>
+          <App />
+        </CountriesProvider>
+      </ThemeProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
