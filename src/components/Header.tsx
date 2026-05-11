b2a36f170c4a300c40194fba7fa98f316b4f47ef
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-11 12:28:03 +0200

    fix(header): improve responsive layout with CSS classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,17 @@ function Header() {
   return (
     <header className="text-grey-950 flex w-full flex-row justify-between bg-white px-4 py-7.5 drop-shadow-sm md:px-10 xl:px-21 dark:bg-blue-900 dark:text-white">
       <Link to={`/`}>
-        <h1 className="text-preset-7">Where in the world?</h1>
+        <h1 className="text-preset-7 xl:text-preset-2">Where in the world?</h1>
       </Link>
       <button
         onClick={toggleTheme}
         type="button"
-        className="text-preset-6-semibold cursor-pointer"
+        className="text-preset-6-semibold xl:text-preset-4-semibold cursor-pointer"
       >
         {isDark ? (
           <span className="flex flex-row items-center gap-2">
             <img
-              className="h-3 w-3"
+              className="aspect-square h-3 w-3 object-cover xl:h-4 xl:w-4"
               src="/images/LightModeSun.png"
               alt="sun symbol "
             />
@@ -25,7 +25,7 @@ function Header() {
         ) : (
           <span className="flex flex-row items-center gap-2">
             <img
-              className="h-3 w-3"
+              className="aspect-square h-3 w-3 object-cover xl:h-4 xl:w-4"
               src="/images/DarkModeIcon.png"
               alt="moon symbol "
             />
