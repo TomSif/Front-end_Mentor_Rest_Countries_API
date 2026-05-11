dbf1e5bfaa1a5d33ed15072ad5a8680a00acdd4d
TomSif <thomas.sifferle.programmer@gmail.com>
2026-05-11 11:49:21 +0200

    feat(country-detail): replace global fetch with alpha endpoint

--- src/components/CountryDetail.tsx
+++ src/components/CountryDetail.tsx
@@ -1,25 +1,51 @@
+import { useEffect, useState } from "react";
 import { useParams, useNavigate, Link } from "react-router";
-import useCountries from "../hooks/useCountries";
+import type { Country } from "../types/index.ts";
 
 function CountryDetail() {
+  const [country, setCountry] = useState<Country | null>(null);
+  const [countriesMap, setCountriesMap] = useState<Country[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   const { alpha3Code } = useParams<{ alpha3Code: string }>();
-  const { countries } = useCountries();
+
   const navigate = useNavigate();
 
-  const findCountry = countries.find(
-    (country) => country.alpha3Code === alpha3Code,
-  );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        const [countryRes, mapRes] = await Promise.all([
+          fetch(
+            `https://restcountries.com/v3.1/alpha/${alpha3Code}?fields=name,tld,cca3,capital,subregion,region,population,borders,languages,currencies,flags`,
+          ),
+          fetch("https://restcountries.com/v3.1/all?fields=cca3,name"),
+        ]);
+
+        const countryData = await countryRes.json();
+        const mapData = await mapRes.json();
+
+        setCountry(countryData);
+        setCountriesMap(mapData);
+      } catch (err) {
+        setError("Erreur lors du fetch");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [alpha3Code]);
+
+  if (loading) return <p>Chargement...</p>;
+
+  if (error) return <p>Country not found</p>;
+
+  if (!country) return <p>Aucun pays trouvé</p>;
 
   const getBorderName = (code: string) =>
-    countries.find((country) => country.alpha3Code === code)?.name;
-
-  if (!findCountry) {
-    return (
-      <main className="bg-grey-50 px-7 py-10">
-        <p className="text-preset-5-light text-grey-950">Country not found</p>
-      </main>
-    );
-  }
+    countriesMap.find((c) => c.cca3 === code)?.name.common;
 
   return (
     <main className="px-7 py-10 md:px-25 dark:text-white">
@@ -35,14 +61,14 @@ function CountryDetail() {
         <section className="w-full max-w-140">
           <img
             className="aspect-4/3 rounded-lg object-cover"
-            src={findCountry.flag}
-            alt={`flag of ${findCountry.name}`}
+            src={country.flags.svg}
+            alt={`flag of ${country.name.common}`}
           />
         </section>
 
         <section>
           <h2 className="text-preset-1 text-grey-950 mb-4 md:mb-6 dark:text-white">
-            {findCountry.name}
+            {country.name.common}
           </h2>
 
           <dl className="flex flex-col gap-8 space-y-1 xl:gap-16">
@@ -51,28 +77,26 @@ function CountryDetail() {
                 <div className="flex gap-1">
                   <dt className="text-preset-9-semibold">Native Name:</dt>
                   <dd className="text-preset-9-light">
-                    {findCountry.nativeName}
+                    {Object.values(country.name.nativeName)[0]?.common}
                   </dd>
                 </div>
                 <div className="flex gap-1">
                   <dt className="text-preset-9-semibold">Population:</dt>
                   <dd className="text-preset-9-light">
-                    {findCountry.population.toLocaleString()}
+                    {country.population.toLocaleString()}
                   </dd>
                 </div>
                 <div className="flex gap-1">
                   <dt className="text-preset-9-semibold">Region:</dt>
-                  <dd className="text-preset-9-light">{findCountry.region}</dd>
+                  <dd className="text-preset-9-light">{country.region}</dd>
                 </div>
                 <div className="flex gap-1">
                   <dt className="text-preset-9-semibold">Sub Region:</dt>
-                  <dd className="text-preset-9-light">
-                    {findCountry.subregion}
-                  </dd>
+                  <dd className="text-preset-9-light">{country.subregion}</dd>
                 </div>
                 <div className="flex gap-1">
                   <dt className="text-preset-9-semibold">Capital:</dt>
-                  <dd className="text-preset-9-light">{findCountry.capital}</dd>
+                  <dd className="text-preset-9-light">{country.capital}</dd>
                 </div>
               </div>
 
@@ -80,19 +104,23 @@ function CountryDetail() {
                 <div className="flex gap-1">
                   <dt className="text-preset-9-semibold">Top Level Domain:</dt>
                   <dd className="text-preset-9-light">
-                    {findCountry?.topLevelDomain.join(", ")}
+                    {country.tld.join(", ")}
                   </dd>
                 </div>
                 <div className="flex gap-1">
                   <dt className="text-preset-9-semibold">Currencies:</dt>
                   <dd className="text-preset-9-light">
-                    {findCountry?.currencies.map((c) => c.name).join(", ")}
+                    {Object.entries(country.currencies).map(
+                      ([code, currency]) => (
+                        <p key={code}>{currency.name}</p>
+                      ),
+                    )}
                   </dd>
                 </div>
                 <div className="flex gap-1">
                   <dt className="text-preset-9-semibold">Languages:</dt>
                   <dd className="text-preset-9-light">
-                    {findCountry?.languages.map((l) => l.name).join(", ")}
+                    {Object.values(country.languages).join(", ")}
                   </dd>
                 </div>
               </div>
@@ -105,25 +133,23 @@ function CountryDetail() {
                 </dt>
                 <dd className="text-preset-9-light flex-2">
                   <ul className="flex flex-wrap gap-4">
-                    {findCountry.borders ? (
-                      findCountry?.borders
-                        .filter((b) => {
-                          return !!getBorderName(b);
-                        })
-                        .map((b) => (
-                          <li key={b}>
-                            <Link
-                              className="btn text-preset-6-light flex h-auto items-center justify-center bg-white px-4 py-1.5 text-center drop-shadow-sm dark:bg-blue-900"
-                              to={`/country/${b}`}
-                            >
-                              {getBorderName(b)}
-                            </Link>
-                          </li>
-                        ))
-                    ) : (
-                      <></>
-                    )}
-                  </ul>
+                    {country.borders?.map((b) => {
+                      const name = getBorderName(b);
+
+                      if (!name) return null;
+
+                      return (
+                        <li key={b}>
+                          <Link
+                            className="btn text-preset-6-light flex h-auto items-center justify-center bg-white px-4 py-1.5 text-center drop-shadow-sm dark:bg-blue-900"
+                            to={`/country/${b}`}
+                          >
+                            {name}
+                          </Link>
+                        </li>
+                      );
+                    })}
+                  </ul>{" "}
                 </dd>
               </div>
             </div>
